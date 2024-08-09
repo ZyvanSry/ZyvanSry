801a970866025b41285079977664fba24effa893
ZyvanSry <loveufromsry@gmail.com>
2024-08-09 13:24:57 +0700

    Add files via upload

---
+++ logical_oparetor.py
@@ -0,0 +1,35 @@
+#____________________________________________#
+#logical operator and 
+#____________________________________________#
+#print( 7==7 and 5<2)
+#x = 100 and print("that guy irritated me ")
+#print(x)
+#b = False and "khos hz "
+#print(b)
+#c = None and "ort mwan ey hah"
+#print(c)
+#g = True and "mean luy ort"
+#print(g)
+#f = 15 and "ort mean teh juii "
+#print(f)
+
+
+
+#____________________________________________#
+            #logical oparetor or#
+#____________________________________________#
+h = 4 <1 or "ort dg"
+print(h)
+i = "hello" or True
+print(i)
+j = "kit mix" and h == True or "ort ban kuit ey teh " 
+print(j)
+
+
+
+print("#____________________________________________#")
+print("            #logical oparetor not#            ")
+print("#____________________________________________#")
+
+k = not not (not None and 10 <= 10  ) or not not (not not 10)
+print(k)
