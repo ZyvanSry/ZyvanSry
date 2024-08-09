13736e7b04f63b1b03f46223d4d9ab61bd24931a
ZyvanSry <loveufromsry@gmail.com>
2024-08-09 13:23:36 +0700

    Add files via upload

---
+++ varieble.py
@@ -0,0 +1,22 @@
+#numeric have two type of num 1. Integer number 
+#                              2.floating number 
+# interger and floating num both have negativr and positive 
+#ch_string 
+#10
+x = 10
+y = -100
+#print(type(x))
+#print(type(y))
+#
+#print("x =",x)
+#print("Fk",x,"#")
+#print(f"something ={x}")
+#print("x"+"y")
+#List = array
+#print(type(x))
+#answer = input("c bay nv = ")
+#print(answer)
+money_bag = int(input("mean tah = "))
+kjey = 10
+sol = money_bag - kjey
+print(f"jit os lmom sol ={sol}$") 
